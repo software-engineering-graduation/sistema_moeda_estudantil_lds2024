a4c0ea1df801a86f012f94d3d546470b4101f196
Lucca Luchi <lucca.luchi@gmail.com>
2024-10-20 20:44:20 -0300

    criação do projeto java

---
+++ implementacao/sistema-de-moeda-estudantil/src/main/java/com/sistema_de_moeda_estudantil/sistema_de_moeda_estudantil/SistemaDeMoedaEstudantilApplication.java
@@ -0,0 +1,13 @@
+package com.sistema_de_moeda_estudantil.sistema_de_moeda_estudantil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeMoedaEstudantilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeMoedaEstudantilApplication.class, args);
+	}
+
+}
---
+++ implementacao/sistema-de-moeda-estudantil/src/test/java/com/sistema_de_moeda_estudantil/sistema_de_moeda_estudantil/SistemaDeMoedaEstudantilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema_de_moeda_estudantil.sistema_de_moeda_estudantil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeMoedaEstudantilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
