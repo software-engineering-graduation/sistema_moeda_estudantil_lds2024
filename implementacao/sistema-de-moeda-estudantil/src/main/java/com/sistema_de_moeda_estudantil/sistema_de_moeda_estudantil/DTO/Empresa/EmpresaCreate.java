3da76a03fef38d655a301d0096e621b0c08b261a
Lucca Luchi <lucca.luchi@gmail.com>
2024-10-21 14:09:27 -0300

    Validações dados de empresa

--- implementacao/sistema-de-moeda-estudantil/src/main/java/com/sistema_de_moeda_estudantil/sistema_de_moeda_estudantil/DTO/Empresa/EmpresaCreate.java
+++ implementacao/sistema-de-moeda-estudantil/src/main/java/com/sistema_de_moeda_estudantil/sistema_de_moeda_estudantil/DTO/Empresa/EmpresaCreate.java
@@ -1,11 +1,23 @@
 package com.sistema_de_moeda_estudantil.sistema_de_moeda_estudantil.DTO.Empresa;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class EmpresaCreate {
+    @NotBlank(message = "O nome é obrigatório")
     private String nome;
+
+    @NotBlank(message = "O email é obrigatório")
+    @Email(message = "Email inválido")
     private String email;
+
+    @NotBlank(message = "A senha é obrigatória")
+    @Size(min = 6, message = "A senha deve ter pelo menos 6 caracteres")
     private String senha;
+
+    @NotBlank(message = "O CNPJ é obrigatório")
     private String cnpj;
 }
